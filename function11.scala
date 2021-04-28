571c1daf68d5a101355de05c8c0616e7364aa54d
TimSpiers-Cwk <82985540+TimSpiers-Cwk@users.noreply.github.com>
2021-04-28 21:50:55 +0100

    Update function11.scala

--- function11.scala
+++ function11.scala
@@ -1,7 +1,3 @@
-sqlContext.sql("CREATE TABLE IF NOT EXISTS places(gplusplaceid STRING, name STRING, price STRING, address STRING, hours STRING, phone STRING, closed STRING) ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t' STORED AS TextFile")
-
-sqlContext("LOAD DATA INPATH 'hdfs://quickstart.cloudera/user/cloudera/places.csv' OVERWRITE INTO TABLE places")
-
 val df = sqlContext.sql("SELECT price as price, COUNT(closed) as OpenClosed FROM places WHERE closed = 'False' AND price LIKE '%$%' GROUP BY price")
 df.show()
 
